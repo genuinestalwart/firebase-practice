35bc355c4da25caeb58c03c929dfc73ac7961324
genuinestalwart <genuinestalwart@gmail.com>
2022-04-10 19:43:36 +0600

    header created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+import Orders from './components/Orders/Orders';
+import Products from './components/Products/Products';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
 	return (
 		<div className="App">
-
+			<Header></Header>
+			<Routes>
+				<Route path='/' element={<Home></Home>}></Route>
+				<Route path='/products' element={<Products></Products>}></Route>
+				<Route path='/orders' element={<Orders></Orders>}></Route>
+				<Route path='/login' element={<SignIn></SignIn>}></Route>
+				<Route path='/register' element={<SignUp></SignUp>}></Route>
+				<Route path='*' element={<NotFound></NotFound>}></Route>
+			</Routes>
 		</div>
 	);
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import logo from '../../logo.svg';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header className='bg-slate-800 flex h-24 items-center justify-between px-40'>
+            <div>
+                <h1 className='font-medium text-3xl text-white'>Ema-John</h1>
+                <div className='flex justify-center text-center'><img className='w-1/2' src={logo} alt="ema john" /></div>
+            </div>
+
+            <nav className='flex justify-between text-white w-2/5'>
+                <Link className='hover:text-amber-400' to='/products'>Products</Link>
+                <Link className='hover:text-amber-400' to='/orders'>Orders</Link>
+                <Link className='hover:text-amber-400' to='/login'>Sign in</Link>
+                <Link className='hover:text-amber-400' to='/register'>Sign up</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
