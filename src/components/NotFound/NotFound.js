50ab45a431a961edce25688a76a5029c372e896b
genuinestalwart <genuinestalwart@gmail.com>
2022-04-13 17:42:46 +0600

    404 page added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import error404 from '../../images/error404.png';
 
 const NotFound = () => {
     return (
-        <div>
-
+        <div className='px-80 text-center'>
+            <div className='my-8'><img className='w-full' src={error404} alt="not found" /></div>
+            <Link to='/'><button className='bg-cyan-400 hover:bg-emerald-400 font-medium p-2 active:ring ring-cyan-400 ring-offset-2 rounded-md text-xl text-white'>Back to Home Page</button></Link>
         </div>
     );
 };
